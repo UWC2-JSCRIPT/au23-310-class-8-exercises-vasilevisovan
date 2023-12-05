5c9e5b1283ccd2215dc645a9ca298b1f68f1853c
vasilevisovan <vasile.visovan@yahoo.com>
2023-12-05 12:04:26 -0800

    #1

--- fetch.js
+++ fetch.js
@@ -1,4 +1,15 @@
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
+
+const MY_API = "7Xlf363eswR0tyqs6Pweik77HWztxaox";
+const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${MY_API}`;
+
+const date = "2021-04-01";
+const list = "hardcover-fiction";
+const booksUrl = `https://api.nytimes.com/svc/books/v3/lists/${date}/${list}.json?api-key=${MY_API}`;
+
+
+
+
 const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 
 const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
--- promise.js
+++ promise.js
@@ -1,14 +1,49 @@
-let myPromise = new Promise(function(resolve, reject) {
-  setTimeout(function() {
-    resolve();
+// let myPromise = new Promise(function(resolve, reject) {
+//   setTimeout(function() {
+//     resolve();
+//   }, 1000);
+// });
+
+// myPromise
+//   .then(function() {
+//     return 99;
+//   })
+//   .then(function(number) {
+//     console.log(number);
+//   });
+
+
+// Create a new promise
+const myPromise = new Promise((resolve, reject) => {
+  // After 1 second, call Math.random()
+  setTimeout(() => {
+      const randomValue = Math.random();
+      
+      // If the result of Math.random() is > 0.5, call resolve()
+      if (randomValue > 0.5) {
+          resolve();
+          console.log(`Your number is: ${randomValue} \u{1F44D}`)
+      } else {
+          // If the result of Math.random() is <= 0.5, call reject()
+          reject();
+          console.log(`Your number is: ${randomValue} \u{1F44D}`)
+      }
   }, 1000);
 });
 
+// Handle the promise resolution
 myPromise
-  .then(function() {
-    return 99;
+  .then(() => {
+      // If the promise is resolved, log 'success'
+      console.log('Success!');
+      console.log('Complete! \u{1F60A}');
   })
-  .then(function(number) {
-    console.log(number);
-  });
-
+  .catch(() => {
+      // If the promise is rejected, log 'fail'
+      console.log('Fail!');
+      console.log('Complete! \u{1F643}');
+  })
+//  .finally(() => {
+//       //In either case, log 'complete'
+//      console.log('Complete :)');
+//  });
